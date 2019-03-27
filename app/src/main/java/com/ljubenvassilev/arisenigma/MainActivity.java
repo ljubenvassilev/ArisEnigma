5741fa6f7917502c09dbb8c6432d52f23b98541f
ljuben.vassilev <ljuben.vassilev@musala.com>
2019-03-27 17:51:24 +0200

    initial commit

---
+++ app/src/main/java/com/ljubenvassilev/arisenigma/MainActivity.java
@@ -0,0 +1,90 @@
+package com.ljubenvassilev.arisenigma;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.view.View;
+import android.widget.EditText;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
+
+    public static final String SAVE_STATE_KEY = "WORDS";
+
+    private EditText editText;
+    private MyRecyclerViewAdapter adapter;
+    private ArrayList<String> words = new ArrayList<>();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyRecyclerViewAdapter(this, words);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scramble();
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putStringArrayList(SAVE_STATE_KEY, words);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        words = savedInstanceState.getStringArrayList(SAVE_STATE_KEY);
+        adapter.update(words);
+    }
+
+    private void scramble() {
+        words = permutation(editText.getText().toString().trim());
+        adapter.update(words);
+    }
+
+    @Override
+    public void onItemClick(View v, int position) {
+        ((ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE))
+                .setPrimaryClip(ClipData.newPlainText("Copied Text", words.get(position)));
+    }
+
+    public static ArrayList<String> permutation(String s) {
+        ArrayList<String> res = new ArrayList<>();
+        if (s.length() == 1) {
+            res.add(s);
+        } else if (s.length() > 1) {
+            int lastIndex = s.length() - 1;
+            String last = s.substring(lastIndex);
+            String rest = s.substring(0, lastIndex);
+            res = merge(permutation(rest), last);
+        }
+        return res;
+    }
+
+    public static ArrayList<String> merge(ArrayList<String> list, String c) {
+        ArrayList<String> res = new ArrayList<>();
+        for (String s : list) {
+            for (int i = 0; i <= s.length(); ++i) {
+                String ps = new StringBuffer(s).insert(i, c).toString();
+                res.add(ps);
+            }
+        }
+        return res;
+    }
+}
---
+++ app/src/main/java/com/ljubenvassilev/arisenigma/MyRecyclerViewAdapter.java
@@ -0,0 +1,68 @@
+package com.ljubenvassilev.arisenigma;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder> {
+
+    private List<String> mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    MyRecyclerViewAdapter(Context context, List<String> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(mInflater.inflate(R.layout.recyclerview_row, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.myTextView.setText(mData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView myTextView;
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            myTextView = itemView.findViewById(R.id.row_content);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (mClickListener != null) mClickListener.onItemClick(v, getAdapterPosition());
+        }
+    }
+
+    void update(List<String> data){
+        mData = data;
+        notifyDataSetChanged();
+    }
+
+    void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    public interface ItemClickListener {
+        void onItemClick(View v, int position);
+    }
+}
